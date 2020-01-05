f179fd6b456eb80ede514ef63afbd94d1dfe675f
Aditya <itsmeadityaagarwal@gmail.com>
2020-01-06 02:31:23 +0530

    add user use case

---
+++ src/usecase/user_usecase/user.go
@@ -0,0 +1,24 @@
+package user_usecase
+
+import (
+	"context"
+	"github.com/itsme/nuc/src/domain/repositories"
+	"github.com/itsme/nuc/src/entities/models"
+)
+
+type User struct {
+	UserRepo repositories.User
+}
+
+func InitUsecase(repo repositories.User)*User{
+	return &User{
+		UserRepo:repo,
+	}
+}
+func (userUC *User) GetUserBySub(ctx context.Context, sub string) (models.User, error) {
+	return userUC.UserRepo.GetUserBySub(ctx, sub)
+}
+
+func (userUC *User) AddUser(ctx context.Context, user models.User) (int64, error) {
+	return userUC.UserRepo.AddUser(ctx, user)
+}
