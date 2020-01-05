fe31998859d8957ad14fbfb051cc2740a586c37f
Aditya <itsmeadityaagarwal@gmail.com>
2020-01-06 02:32:42 +0530

    add models

---
+++ src/entities/models/cart.go
@@ -0,0 +1,47 @@
+package models
+
+type Cart struct {
+	ID        int64 `json:"id" db:"id"`
+	UserID    int64 `json:"user_id" db:"user_id"`
+	Status    int   `json:"status" db:"status"`
+	CreatedAt int64 `json:"created_at" db:"created_at"`
+	UpdatedAt int64 `json:"updated_at" db:"updated_at"`
+}
+
+type CartItems struct {
+	ID        int64 `json:"id" db:"id"`
+	CartID    int64 `json:"cart_id" db:"cart_id"`
+	ProductID int64 `json:"product_id" db:"product_id"`
+	Qty       int64 `json:"qty" db:"qty"`
+	Status    int   `json:"status" db:"status"`
+	CreatedAt int64 `json:"created_at" db:"created_at"`
+	UpdatedAt int64 `json:"updated_at" db:"updated_at"`
+}
+
+type CartXCartItems struct {
+	CartID     int64 `json:"cart_id" db:"cart_id"`
+	UserID     int64 `json:"user_id" db:"user_id"`
+	ProductID  int64 `json:"product_id" db:"product_id"`
+	Qty        int64 `json:"qty" db:"qty"`
+	ItemStatus int   `json:"item_status" db:"item_status"`
+	CreatedAt  int64 `json:"created_at" db:"created_at"`
+	UpdatedAt  int64 `json:"updated_at" db:"updated_at"`
+}
+
+type CartDetail struct {
+	CartID   int64     `json:"cart_id" db:"cart_id"`
+	UserID   int64     `json:"user_id" db:"user_id"`
+	TotalPrice float64
+	Products []CartProduct `json:"products"`
+}
+type CartProduct struct {
+	Product
+	Qty int64 `json:"qty" db:"qty"`
+	TotalPrice float64
+
+}
+
+type User struct{
+	ID int64 `db:"id"`
+	Sub string `db:"sub"`
+}
---
+++ src/entities/models/product.go
@@ -0,0 +1,91 @@
+package models
+
+type Product struct { //ProductDetail
+	ID     int64      `json:"id"`
+	Images []string `json:"images"`
+	Name   string   `json:"name"`
+	Slug   string   `json:"slug"`
+	Status string   `json:"status"`
+	Price  float64  `json:"price"`
+	Offers []struct {
+		Price float64 `json:"price"`
+	} `json:"offers"`
+}
+
+type ProductDetailResponse struct {
+	Code int `json:"code"`
+	Data struct {
+		Page struct {
+			Entity struct {
+				Product
+			} `json:"entity"`
+		} `json:"page"`
+	} `json:"data"`
+	Status string `json:"status"`
+}
+
+type ProductListResponse struct {
+	Code int `json:"code"`
+	Data struct {
+		Page struct {
+			Layouts []struct {
+				Value struct {
+					Collection []struct {
+						Products []Product   `json:"product"`
+						Category interface{} `json:"category"`
+					} `json:"collection"`
+				} `json:"value"`
+			} `json:"layouts"`
+		} `json:"page"`
+	} `json:"data"`
+	Status string `json:"status"`
+}
+
+type ProductListResponse1 struct {
+	Code int `json:"code"`
+	Data struct {
+		Page struct {
+			Layouts []struct {
+				Value struct {
+					Collection interface{} `json:"collection"`
+				} `json:"value"`
+			} `json:"layouts"`
+		} `json:"page"`
+	} `json:"data"`
+	Status string `json:"status"`
+}
+type ProductListResponse2 struct {
+	Code int `json:"code"`
+	Data struct {
+		Page struct {
+			Layouts []struct {
+				Value struct {
+					Collection []struct {
+						Products []Product   `json:"product"`
+						Category interface{} `json:"category"`
+					} `json:"collection"`
+				} `json:"value"`
+			} `json:"layouts"`
+		} `json:"page"`
+	} `json:"data"`
+	Status string `json:"status"`
+}
+
+type Collection struct {
+	Count interface {
+	} `json:"count"`
+	Filters interface {
+	} `json:"filters"`
+	Limit interface {
+	} `json:"limit"`
+	Offset interface {
+	} `json:"offset"`
+	Products []Product `json:"product"`
+}
+
+type UserGoogle struct {
+	Sub           string `json:"sub"`
+	Picture       string `json:"picture"`
+	Email         string `json:"email"`
+	EmailVerified bool   `json:"email_verified"`
+}
